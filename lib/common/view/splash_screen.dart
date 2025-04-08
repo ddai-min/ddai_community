0d68c23713ac9bc11a5070ee7d893c40adcd1948
권택민 <yoda3708@gmail.com>
2025-04-08 17:36:34 +0900

    add: 업데이트 확인 로직

--- lib/common/view/splash_screen.dart
+++ lib/common/view/splash_screen.dart
@@ -1,11 +1,16 @@
+import 'dart:io';
+
+import 'package:ddai_community/common/component/default_dialog.dart';
 import 'package:ddai_community/common/const/colors.dart';
 import 'package:ddai_community/common/layout/default_layout.dart';
 import 'package:ddai_community/common/view/home_tab.dart';
 import 'package:ddai_community/user/provider/user_me_provider.dart';
 import 'package:ddai_community/user/view/login_screen.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class SplashScreen extends ConsumerStatefulWidget {
   static get routeName => 'splash';
@@ -22,13 +27,9 @@ class _SplashScreenState extends ConsumerState<SplashScreen> {
     super.initState();
 
     Future.delayed(
-      const Duration(seconds: 2),
+      const Duration(seconds: 1),
       () {
-        context.goNamed(
-          ref.read(userMeProvider.notifier).state.id == ''
-              ? LoginScreen.routeName
-              : HomeTab.routeName,
-        );
+        _pushMain();
       },
     );
   }
@@ -49,5 +50,63 @@ class _SplashScreenState extends ConsumerState<SplashScreen> {
         ),
       ),
     );
+  }
+
+  void _pushMain() async {
+    await _checkUpdate();
+
+    context.goNamed(
+      ref.read(userMeProvider.notifier).state.id == ''
+          ? LoginScreen.routeName
+          : HomeTab.routeName,
+    );
+  }
+
+  Future<void> _checkUpdate() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+
+    final remoteConfig = FirebaseRemoteConfig.instance;
+
+    await remoteConfig.fetchAndActivate().then((value) async {
+      final currentVersionName = packageInfo.version;
+      final currentVersionCode = int.parse(packageInfo.buildNumber);
+
+      final latestVersionName = remoteConfig.getString('version_name');
+      final latestVersionCode = remoteConfig.getInt('version_code');
+      final isForce = remoteConfig.getBool('is_force');
+
+      if (isForce) {
+        if (currentVersionName != latestVersionName ||
+            currentVersionCode < latestVersionCode) {
+          await showDialog(
+            context: context,
+            barrierDismissible: false,
+            builder: (context) {
+              return DefaultDialog(
+                contentText: '최신 버전으로 업데이트 해주세요.\n\n업데이트 후 이용 가능합니다.',
+                buttonText: '확인',
+                onPressed: () {
+                  exit(0);
+                },
+              );
+            },
+          );
+        }
+      }
+    }).catchError((error) async {
+      await showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) {
+          return DefaultDialog(
+            contentText: '업데이트를 확인할 수 없습니다.\n\n앱을 종료합니다.',
+            buttonText: '확인',
+            onPressed: () {
+              exit(0);
+            },
+          );
+        },
+      );
+    });
   }
 }
